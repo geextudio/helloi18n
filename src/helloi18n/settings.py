b804bb217121b51284b5aecb00c6385495bbdbd4
Yang SHEN <geextudio@outlook.com>
2022-05-30 02:36:50 +0800

    Update settings.py

--- src/helloi18n/settings.py
+++ src/helloi18n/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cb&&c*$wdwh=%^b-_-3mf=i8*_n+=7s#g1c04nbb20=zr+lpvj'
+SECRET_KEY = 'django-insecure-8_k0xt2ce@xtt(^j_#7dzn9uu7dk2(1k1+cob58x!eo@&fkmc('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -53,7 +53,6 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'helloi18n.urls'
 
 TEMPLATES = [
-    
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
